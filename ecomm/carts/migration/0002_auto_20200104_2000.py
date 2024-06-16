ebc34b522a6ed2d4c17edce0d9ed14c3670fa67f
shrasti-pro <gshrashti022@gmail.com>
2024-06-16 19:54:33 +0530

    Create 0002_auto_20200104_2000.py

---
+++ ecomm/carts/migration/0002_auto_20200104_2000.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('carts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='cart',
+            old_name='product',
+            new_name='products',
+        ),
+    ]
